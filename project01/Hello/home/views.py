9e83c04de1714bb5d85e08f176f5d8e5ba7700a7
dhruv <87694090+code-with-dhruv@users.noreply.github.com>
2022-11-20 20:31:52 +0530

    Update views.py

--- project01/Hello/home/views.py
+++ project01/Hello/home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,HttpResponse,redirect
+from django.shortcuts import render, HttpResponse, redirect
 
 from django.http import HttpResponseBadRequest
 from django import forms
@@ -7,25 +7,42 @@ from django.db.models import Q
 import django_excel as excel
 from home.models import Excel
 import csv
+
+csv_f="a"
 # Create your views here.
 def index(request):
-    data=Excel.objects.all()
+    data = Excel.objects.all()
     if 'q' in request.GET:
-        q=request.GET['q']
-        Excel.objects.filter(Q(Title_icontains=q)|Q(authors_icontains=q)|Q(dbid_icontains=q))
-    
-    return render(request,'index.html',{'data':data})
+        q = request.GET['q']
+        Excel.objects.filter(
+            Q(Title_icontains=q) | Q(authors_icontains=q)
+            | Q(dbid_icontains=q))
+
+    return render(request, 'index.html', {'data': data})
 
 
 def search(request):
-    query=request.GET['query']
+    global csv_f
+    query = request.GET['query']
     # allPosts=Excel.objects.all()
-    allPosts=Excel.objects.filter(Q(Title__icontains=query)|Q(authors__icontains=query)|Q(dbid__icontains=query))
+    allPosts = Excel.objects.filter(
+        Q(Title__icontains=query) | Q(authors__icontains=query)
+        | Q(dbid__icontains=query))
+  
+    csv_f=allPosts
+    params = {'allPosts': allPosts}
 
-    params={'allPosts':allPosts}
+    return render(request, 'search.html', params)
 
-    return render(request,'search.html',params)
 
-# def venue(request):
-#     response=HttpResponse(content_type='text/csv')
-#     response['content']
+def venue_csv(request):
+    global csv_f
+    response = HttpResponse(content_type="text/csv")
+    response['Content-Disposition'] = 'attachment,filename=venues.text'
+    writer = csv.writer(response)
+    venues = Excel.objects.all()
+    writer.writerow(['Title', 'Author', 'WOSid'])
+    for i in csv_f:
+        writer.writerow([i.Title, i.authors, i.dbid])
+
+    return response
